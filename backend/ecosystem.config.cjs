79cab467c50d3847f768ab1606d86cd236b0a4fc
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2025-05-10 15:19:30 -0500

    testing

--- backend/ecosystem.config.cjs
+++ backend/ecosystem.config.cjs
@@ -1,18 +1,9 @@
 module.exports = {
-  apps: [
-    {
-      name: 'melting-backend',
-      script: './server.js',
-      env: {
-        NODE_ENV: 'development',
-        CLIENT_URL: 'http://localhost:5173',
-        SERVER_URL: 'http://localhost:3001',
-      },
-      env_production: {
-        NODE_ENV: 'production',
-        CLIENT_URL: 'https://meltingpointproductions.com',
-        SERVER_URL: 'https://meltingpointproductions.com',
-      },
-    },
-  ],
+  apps: [{
+    name: "melting-backend",
+    script: "server.js",
+    env_production: {
+      NODE_ENV: "production"
+    }
+  }]
 };
