414ea77792630ce4e854243cc14c69dfce4f42b4
WilliM233 <30969699+WilliM233@users.noreply.github.com>
2025-05-10 13:50:42 -0500

    Update cookie settings

--- backend/server.js
+++ backend/server.js
@@ -27,8 +27,9 @@ app.use(
       saveUninitialized: false,
       cookie: {
         secure: process.env.NODE_ENV === 'production',
+        httpOnly: process.env.NODE_ENV === 'production',
         sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-        domain: process.env.NODE_ENV === 'production' ? 'meltingpointproductions.com' : undefined
+        domain: process.env.NODE_ENV === 'production' ? '.meltingpointproductions.com' : undefined
       },
     })
   );
